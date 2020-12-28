4f80bb656a294da99f2d704e7e98281b2f416f11
erik <eovermeire@gmail.com>
2020-12-28 14:39:12 +0100

    setting mem growth = true for gpu

--- dcgan/dcgan.py
+++ dcgan/dcgan.py
@@ -46,6 +46,19 @@ class DCGAN:
         self.losses = {}
         self.progress = {}
 
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        if gpus:
+            try:
+                # Currently, memory growth needs to be the same across GPUs
+                for gpu in gpus:
+                    tf.config.experimental.set_memory_growth(gpu, True)
+                    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+            except RuntimeError as e:
+                # Memory growth must be set before GPUs have been initialized
+                print(e)
+
+
         # load dataset
         self.dataset_pipeline = DatasetPipeline(self.dataset_path,
                                                 self.image_size,

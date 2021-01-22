70dd1585183c0f541ab16f84b2e61fe9a3e83539
lifthoofd <eovermeire@gmail.com>
2021-01-22 12:13:27 +0100

    removed kernel initializer from checkpoint

--- dcgan/dcgan.py
+++ dcgan/dcgan.py
@@ -91,8 +91,7 @@ class DCGAN:
                                               generator_optimizer=self.generator_optimizer,
                                               discriminator_optimizer=self.discriminator_optimizer,
                                               generator=self.generator,
-                                              discriminator=self.discriminator,
-                                              weight_init=self.weight_init)
+                                              discriminator=self.discriminator)
         self.checkpoint_manager = tf.train.CheckpointManager(self.checkpoint, self.ckpt_path, max_to_keep=10)
 
         self.summary_writer = tf.summary.create_file_writer(self.summary_path)

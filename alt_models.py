32446b85cc07e0bf986cc711bbf06ed2c2e04b43
lifthoofd <eovermeire@gmail.com>
2022-08-29 13:35:53 +0200

    adjusted to tf2.9

--- alt_models.py
+++ alt_models.py
@@ -1,154 +1,155 @@
-from tensorflow.python.keras import layers
-from tensorflow.python.keras import models
+# from tensorflow.keras import layers
+# from tensorflow.keras import models
+from tensorflow import keras
 from math import log, pow
 
 
 def make_generator_model(y_dim, z_dim, weight_init, bn_momentum, image_size, aspect_ratio, filters=64):
-    z = layers.Input(shape=(1, 1, z_dim))
-    y = layers.Input(shape=(1, 1, y_dim,))
+    z = keras.layers.Input(shape=(1, 1, z_dim))
+    y = keras.layers.Input(shape=(1, 1, y_dim,))
 
-    gen_in = layers.concatenate([z, y], axis=3)
+    gen_in = keras.layers.concatenate([z, y], axis=3)
 
     start_size = (1, 2)
 
-    x = layers.Dense(start_size[0] * start_size[1] * 1024)(gen_in)
-    x = layers.Reshape((start_size[0], start_size[1], 1024))(x)
-    x = layers.ReLU()(x)
+    x = keras.layers.Dense(start_size[0] * start_size[1] * 4096)(gen_in)
+    x = keras.layers.Reshape((start_size[0], start_size[1], 4096))(x)
+    x = keras.layers.ReLU()(x)
 
     # 2, 4
-    x = layers.Conv2DTranspose(1024,
+    x = keras.layers.Conv2DTranspose(4096,
                                (3, 3),
                                strides=(2, 2),
                                padding='same',
                                use_bias=False,
                                kernel_initializer=weight_init)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.ReLU()(x)
 
     # 4, 8
-    x = layers.Conv2DTranspose(512,
+    x = keras.layers.Conv2DTranspose(2048,
                                (3, 3),
                                strides=(2, 2),
                                padding='same',
                                use_bias=False,
                                kernel_initializer=weight_init)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.ReLU()(x)
 
     # 8, 16
-    x = layers.Conv2DTranspose(256,
+    x = keras.layers.Conv2DTranspose(1024,
                                (3, 3),
                                strides=(2, 2),
                                padding='same',
                                use_bias=False,
                                kernel_initializer=weight_init)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.ReLU()(x)
 
     # 16, 32
-    x = layers.Conv2DTranspose(128,
+    x = keras.layers.Conv2DTranspose(512,
                                (3, 3),
                                strides=(2, 2),
                                padding='same',
                                use_bias=False,
                                kernel_initializer=weight_init)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.ReLU()(x)
 
     # 32, 64
-    x = layers.Conv2DTranspose(64,
+    x = keras.layers.Conv2DTranspose(256,
                                (3, 3),
                                strides=(2, 2),
                                padding='same',
                                use_bias=False,
                                kernel_initializer=weight_init)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.ReLU()(x)
 
     # 64, 128
-    x = layers.Conv2DTranspose(32,
+    x = keras.layers.Conv2DTranspose(128,
                                (3, 3),
                                strides=(2, 2),
                                padding='same',
                                use_bias=False,
                                kernel_initializer=weight_init)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.ReLU()(x)
 
-    # # 128, 256
-    # x = layers.Conv2DTranspose(64,
-    #                            (3, 3),
-    #                            strides=(2, 2),
-    #                            padding='same',
-    #                            use_bias=False,
-    #                            kernel_initializer=weight_init)(x)
-    # x = layers.BatchNormalization()(x)
-    # x = layers.ReLU()(x)
-    #
-    # # 256, 512
-    # x = layers.Conv2DTranspose(32,
-    #                            (3, 3),
-    #                            strides=(2, 2),
-    #                            padding='same',
-    #                            use_bias=False,
-    #                            kernel_initializer=weight_init)(x)
-    # x = layers.BatchNormalization()(x)
-    # x = layers.ReLU()(x)
+    # 128, 256
+    x = keras.layers.Conv2DTranspose(64,
+                               (3, 3),
+                               strides=(2, 2),
+                               padding='same',
+                               use_bias=False,
+                               kernel_initializer=weight_init)(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.ReLU()(x)
 
-    x = layers.Conv2DTranspose(3, (3, 3), strides=(1, 1), padding='same', activation='tanh', use_bias=False,
+    # 256, 512
+    x = keras.layers.Conv2DTranspose(32,
+                               (3, 3),
+                               strides=(2, 2),
+                               padding='same',
+                               use_bias=False,
+                               kernel_initializer=weight_init)(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.ReLU()(x)
+
+    x = keras.layers.Conv2DTranspose(3, (3, 3), strides=(1, 1), padding='same', activation='tanh', use_bias=False,
                                kernel_initializer=weight_init)(x)
 
-    return models.Model([z, y], x, name='generator')
+    return keras.models.Model([z, y], x, name='generator')
 
 
 def make_discriminator_model(y_dim, weight_init, image_size, lr_slope, aspect_ratio, filters=64):
-    im = layers.Input(shape=(image_size[0], image_size[1], 3))
-    y = layers.Input(shape=(image_size[0], image_size[1], y_dim))
+    im = keras.layers.Input(shape=(image_size[0], image_size[1], 3))
+    y = keras.layers.Input(shape=(image_size[0], image_size[1], y_dim))
 
-    x = layers.concatenate([im, y], axis=3)
+    x = keras.layers.concatenate([im, y], axis=3)
 
-    # # 128, 256
-    # x = layers.Conv2D(32, (3, 3), strides=(2, 2), padding='same', use_bias=False, kernel_initializer=weight_init)(x)
-    # # x = layers.LayerNormalization()(x)
-    # x = layers.LeakyReLU()(x)
+    # 128, 256
+    x = keras.layers.Conv2D(32, (3, 3), strides=(2, 2), padding='same', use_bias=False, kernel_initializer=weight_init)(x)
+    # x = keras.layers.LayerNormalization()(x)
+    x = keras.layers.LeakyReLU()(x)
 
-    # # 64, 128
-    # x = layers.Conv2D(32, (3, 3), strides=(2, 2), padding='same', use_bias=False, kernel_initializer=weight_init)(x)
-    # # x = layers.LayerNormalization()(x)
-    # x = layers.LeakyReLU()(x)
+    # 64, 128
+    x = keras.layers.Conv2D(64, (3, 3), strides=(2, 2), padding='same', use_bias=False, kernel_initializer=weight_init)(x)
+    # x = keras.layers.LayerNormalization()(x)
+    x = keras.layers.LeakyReLU()(x)
 
     # 32, 64
-    x = layers.Conv2D(32, (3, 3), strides=(2, 2), padding='same', use_bias=False, kernel_initializer=weight_init)(x)
-    # x = layers.LayerNormalization()(x)
-    x = layers.LeakyReLU()(x)
+    x = keras.layers.Conv2D(128, (3, 3), strides=(2, 2), padding='same', use_bias=False, kernel_initializer=weight_init)(x)
+    # x = keras.layers.LayerNormalization()(x)
+    x = keras.layers.LeakyReLU()(x)
 
     # 16, 32
-    x = layers.Conv2D(64, (3, 3), strides=(2, 2), padding='same', use_bias=False, kernel_initializer=weight_init)(x)
-    # x = layers.LayerNormalization()(x)
-    x = layers.LeakyReLU()(x)
+    x = keras.layers.Conv2D(256, (3, 3), strides=(2, 2), padding='same', use_bias=False, kernel_initializer=weight_init)(x)
+    # x = keras.layers.LayerNormalization()(x)
+    x = keras.layers.LeakyReLU()(x)
 
     # 8, 16
-    x = layers.Conv2D(128, (3, 3), strides=(2, 2), padding='same', use_bias=False, kernel_initializer=weight_init)(x)
-    # x = layers.LayerNormalization()(x)
-    x = layers.LeakyReLU()(x)
+    x = keras.layers.Conv2D(512, (3, 3), strides=(2, 2), padding='same', use_bias=False, kernel_initializer=weight_init)(x)
+    # x = keras.layers.LayerNormalization()(x)
+    x = keras.layers.LeakyReLU()(x)
 
     # 4, 8
-    x = layers.Conv2D(256, (3, 3), strides=(2, 2), padding='same', use_bias=False, kernel_initializer=weight_init)(x)
-    # x = layers.LayerNormalization()(x)
-    x = layers.LeakyReLU()(x)
+    x = keras.layers.Conv2D(1024, (3, 3), strides=(2, 2), padding='same', use_bias=False, kernel_initializer=weight_init)(x)
+    # x = keras.layers.LayerNormalization()(x)
+    x = keras.layers.LeakyReLU()(x)
 
     # 2, 4
-    x = layers.Conv2D(512, (3, 3), strides=(2, 2), padding='same', use_bias=False, kernel_initializer=weight_init)(x)
-    # x = layers.LayerNormalization()(x)
-    x = layers.LeakyReLU()(x)
+    x = keras.layers.Conv2D(2048, (3, 3), strides=(2, 2), padding='same', use_bias=False, kernel_initializer=weight_init)(x)
+    # x = keras.layers.LayerNormalization()(x)
+    x = keras.layers.LeakyReLU()(x)
 
     # 1, 2
-    x = layers.Conv2D(1024, (3, 3), strides=(2, 2), padding='same', use_bias=False, kernel_initializer=weight_init)(x)
-    # x = layers.LayerNormalization()(x)
-    x = layers.LeakyReLU()(x)
+    x = keras.layers.Conv2D(4096, (3, 3), strides=(2, 2), padding='same', use_bias=False, kernel_initializer=weight_init)(x)
+    # x = keras.layers.LayerNormalization()(x)
+    x = keras.layers.LeakyReLU()(x)
 
-    x = layers.Flatten()(x)
-    x = layers.Dense(1)(x)
+    x = keras.layers.Flatten()(x)
+    x = keras.layers.Dense(1)(x)
 
-    return models.Model([im, y], x, name='discriminator')
+    return keras.models.Model([im, y], x, name='discriminator')
 
